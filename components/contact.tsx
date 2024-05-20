a03117772b5a9896d112357006476b99b679a367
jatin51997 <jatin51997@gmail.com>
2024-05-20 16:29:10 -0400

    revamped design

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,114 +1,114 @@
-"use client";
+// "use client";
 
-import React, { ChangeEvent, useState } from "react";
-import SectionHeading from "./SectionHeading";
-import { motion } from "framer-motion";
-import { useSectionInView } from "@/lib/hooks";
-import SubmitBtn from "./submitButton";
-import toast from "react-hot-toast";
-import Head from "next/head";
+// import React, { ChangeEvent, useState } from "react";
+// import SectionHeading from "./SectionHeading";
+// import { motion } from "framer-motion";
+// import { useSectionInView } from "@/lib/hooks";
+// import SubmitBtn from "./submitButton";
+// import toast from "react-hot-toast";
+// import Head from "next/head";
 
-interface FormData {
-  senderEmail: string;
-  message: string;
-}
+// interface FormData {
+//   senderEmail: string;
+//   message: string;
+// }
 
-export default function Contact() {
-  const { ref } = useSectionInView("Contact");
-  const [formData, setFormData] = useState<FormData>({
-    senderEmail: "",
-    message: "",
-  });
+// export default function Contact() {
+//   const { ref } = useSectionInView("Contact");
+//   const [formData, setFormData] = useState<FormData>({
+//     senderEmail: "",
+//     message: "",
+//   });
 
-  const handleInputChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+//   const handleInputChange = (
+//     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+//   ) => {
+//     const { name, value } = e.target;
+//     setFormData({
+//       ...formData,
+//       [name]: value,
+//     });
+//   };
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    const resp = await fetch("api/send", {
-      method: "Post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-    if (resp.status === 200) {
-      toast.success("Email sent successfully!");
-    } else {
-      toast.error("Network Issue");
-    }
-  };
+//   const handleSubmit = async (e: any) => {
+//     e.preventDefault();
+//     const resp = await fetch("api/send", {
+//       method: "Post",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(formData),
+//     });
+//     if (resp.status === 200) {
+//       toast.success("Email sent successfully!");
+//     } else {
+//       toast.error("Network Issue");
+//     }
+//   };
 
-  return (
-    <motion.section
-      id="contact"
-      ref={ref}
-      className="mb-20 sm:mb-28 w-full sm:w-3/4 mx-auto text-center "
-      initial={{
-        opacity: 0,
-      }}
-      whileInView={{
-        opacity: 1,
-      }}
-      transition={{
-        duration: 1,
-      }}
-      viewport={{
-        once: true,
-      }}
-    >
-      <SectionHeading>Contact me</SectionHeading>
+//   return (
+//     <motion.section
+//       id="contact"
+//       ref={ref}
+//       className="mb-20 sm:mb-28 w-full sm:w-3/4 mx-auto text-center "
+//       initial={{
+//         opacity: 0,
+//       }}
+//       whileInView={{
+//         opacity: 1,
+//       }}
+//       transition={{
+//         duration: 1,
+//       }}
+//       viewport={{
+//         once: true,
+//       }}
+//     >
+//       <SectionHeading>Contact me</SectionHeading>
 
-      <p className="text-gray-700 -mt-6 dark:text-white/80">
-        Please contact me directly at{" "}
-        <a className="underline" href="mailto:jatinnaroraa5@gmail.com">
-          jatinnaroraa5@gmail.com
-        </a>{" "}
-        or through this form.
-      </p>
+//       <p className="text-gray-700 -mt-6 dark:text-white/80">
+//         Please contact me directly at{" "}
+//         <a className="underline" href="mailto:jatinnaroraa5@gmail.com">
+//           jatinnaroraa5@gmail.com
+//         </a>{" "}
+//         or through this form.
+//       </p>
 
-      <form
-        className="mt-10 flex flex-col dark:text-black"
-        // action={async (formData) => {
-        //   const { data, error } = await sendEmail(formData);
+//       <form
+//         className="mt-10 flex flex-col dark:text-black"
+//         // action={async (formData) => {
+//         //   const { data, error } = await sendEmail(formData);
 
-        //   if (error) {
-        //     toast.error(error);
-        //     return;
-        //   }
+//         //   if (error) {
+//         //     toast.error(error);
+//         //     return;
+//         //   }
 
-        //   toast.success("Email sent successfully!");
-        // }}
-        onSubmit={handleSubmit}
-      >
-        <input
-          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-          name="senderEmail"
-          type="email"
-          required
-          maxLength={500}
-          placeholder="Your email"
-          value={formData.senderEmail}
-          onChange={handleInputChange}
-        />
-        <textarea
-          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-          name="message"
-          placeholder="Your message"
-          required
-          maxLength={5000}
-          value={formData.message}
-          onChange={handleInputChange}
-        />
-        <SubmitBtn />
-      </form>
-    </motion.section>
-  );
-}
+//         //   toast.success("Email sent successfully!");
+//         // }}
+//         onSubmit={handleSubmit}
+//       >
+//         <input
+//           className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+//           name="senderEmail"
+//           type="email"
+//           required
+//           maxLength={500}
+//           placeholder="Your email"
+//           value={formData.senderEmail}
+//           onChange={handleInputChange}
+//         />
+//         <textarea
+//           className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+//           name="message"
+//           placeholder="Your message"
+//           required
+//           maxLength={5000}
+//           value={formData.message}
+//           onChange={handleInputChange}
+//         />
+//         <SubmitBtn />
+//       </form>
+//     </motion.section>
+//   );
+// }
